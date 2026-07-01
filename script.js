095c957e54e2ac3a578bc063feef5fffe7b4dbff
RiveraCamila <camila.rivera.polanco@gmail.com>
2026-06-30 21:09:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function abrirMBT() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("mbt").style.display = "block";
+}
+
+function volverHome() {
+    document.getElementById("mbt").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
+
 function abrirCatastro() {
-    alert("Entrando a módulo Catastro Predial");
+    document.getElementById("mbt").style.display = "none";
+    document.getElementById("catastro").style.display = "block";
+}
+
+function volverMBT() {
+    document.getElementById("catastro").style.display = "none";
+    document.getElementById("mbt").style.display = "block";
+}
+
+function guardarRegistro() {
+    document.getElementById("catastro").style.display = "none";
+    document.getElementById("confirmacion").style.display = "block";
+}
+
+function volverHomeDesdeConfirmacion() {
+    document.getElementById("confirmacion").style.display = "none";
+    document.getElementById("home").style.display = "block";
 }
